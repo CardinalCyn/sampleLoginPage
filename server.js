df0c893af5d19c1ee8b5c64d49272523762a0415
Salah Zanabili <Zanabilis@gmail.com>
2022-05-30 03:25:11 -0500

    1st commit

--- server.js
+++ server.js
@@ -3,16 +3,39 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session=require('express-session');
+const passport=require('passport');
+const routes = require('./routes');
+const auth = require('./auth.js');
 
 const app = express();
+//sets the view engine to pug
+app.set("view engine",'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//used to enable express-session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+app.use(passport.initialize());
+app.use(passport.session());
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+  routes(app,myDataBase);
+  auth(app,myDataBase);
+  
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
